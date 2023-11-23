80d051bdbb4174e6bd9687e3a5a53a71eb808ba6
Gabriel Santos <gabrielcarneirocsantos@gmail.com>
2023-11-23 14:43:21 -0300

    Alterando logica na consulta

--- src/model/entities/PhoneBook.java
+++ src/model/entities/PhoneBook.java
@@ -28,9 +28,11 @@ public class PhoneBook {
 
     public String showContact(String name){
         for(int i = 0; i < listContacts.length; i++){
-            if(listContacts[i].getName().equals(name)){
-                String contactName = listContacts[i].getName();
-                return contactName;
+            if(listContacts[i] != null){
+                if(listContacts[i].getName().equals(name)){
+                    String contactName = listContacts[i].getName();
+                    return contactName;
+                }
             }
         }
         throw new ContactException("Não existe esse contato");
