1191aaefa8e6d23dc31b2f1a2542c3909cc0e4fd
Gabriel Santos <gabrielcarneirocsantos@gmail.com>
2023-11-23 11:22:44 -0300

    Classe PhoneBook

---
+++ src/entities/PhoneBook.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class PhoneBook {
+    private static int contador;
+    private Contact[] listContact = new Contact[3];
+
+    public void addContact(Contact contact){
+        listContact[contador] = contact;
+        contador++;
+    }
+
+    public Contact listContacts(){
+        for(int i = 0; i < listContact.length; i++){
+            return listContact[i];
+        }
+        return null;
+    }
+}
