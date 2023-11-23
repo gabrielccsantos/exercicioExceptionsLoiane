2d75fd1e3daccac8ab999f7598571b70a778fbf1
Gabriel Santos <gabrielcarneirocsantos@gmail.com>
2023-11-23 13:53:26 -0300

    Refatorando a classe PhoneBook

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,13 @@
 package application;
 
+import model.contactException.ContactException;
+import model.entities.Contact;
+import model.entities.PhoneBook;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
--- src/model/entities/PhoneBook.java
+++ src/model/entities/PhoneBook.java
@@ -1,17 +1,5 @@
-package entities;
+package model.entities;
 
 public class PhoneBook {
-    private static int contador;
-    private Contact[] listContact = new Contact[3];
 
-    public void addContact(Contact contact){
-        listContact[contador] = contact;
-        contador++;
-    }
-
-    public Contact listContacts(){
-       for(int i = 0; i < listContact.length; i++){
-           return listContact[i];
-       }
-    }
 }
