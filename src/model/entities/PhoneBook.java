576d51ac780b9b861a440c89ccc05d153991dfa0
Gabriel Santos <gabrielcarneirocsantos@gmail.com>
2023-11-23 14:19:49 -0300

    Classe PhoneBook

--- src/model/entities/PhoneBook.java
+++ src/model/entities/PhoneBook.java
@@ -1,5 +1,38 @@
 package model.entities;
 
-public class PhoneBook {
+import model.contactException.ContactException;
 
+public class PhoneBook {
+    private Contact[] listContacts;
+
+
+    public PhoneBook(){
+        listContacts = new Contact[5];
+    }
+
+    public void addContact(Contact contact){
+        boolean emptyList = true;
+
+        for(int i = 0; i < listContacts.length; i++){
+            if(listContacts[i] != null){
+                listContacts[i] = contact;
+                emptyList = false;
+            }
+        }
+        emptyList = true;
+
+        if(emptyList){
+            throw new ContactException("Lista está cheia");
+        }
+    }
+
+    public String showContact(String name){
+        for(int i = 0; i < listContacts.length; i++){
+            if(listContacts[i].getName().equals(name)){
+                String contactName = listContacts[i].getName();
+                return contactName;
+            }
+        }
+        throw new ContactException("Não existe esse contato");
+    }
 }
