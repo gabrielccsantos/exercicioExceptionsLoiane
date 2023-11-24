e2ff476d8ab88a35fa5c58da0642e66cd700fc3d
Gabriel Santos <gabrielcarneirocsantos@gmail.com>
2023-11-24 12:29:12 -0300

    Class PhoneBook com mudanÃcas na logica

--- src/model/entities/PhoneBook.java
+++ src/model/entities/PhoneBook.java
@@ -1,9 +1,9 @@
 package model.entities;
 
-import model.contactException.ContactException;
+import model.exception.ContactException;
 
 public class PhoneBook {
-    private final Contact[] listContacts;
+    private Contact[] listContacts;
 
 
     public PhoneBook(){
@@ -11,8 +11,27 @@ public class PhoneBook {
     }
 
     public void addContact(Contact contact){
+        boolean fullList = true;
+        for(int i = 0; i < listContacts.length; i++){
+            if(listContacts[i] == null){
+                listContacts[i] = contact;
+                fullList = false;
+                break;
+            }
+        }
+        if(fullList){
+            throw new ContactException("Agenda cheia");
+        }
     }
 
     public String showContact(String name){
+         for (int i = 0; i < listContacts.length; i++){
+             if(listContacts[i] != null){
+                 if (listContacts[i].getName().equals(name)){
+                     return listContacts[i].getPhone();
+                 }
+             }
+         }
+         throw new ContactException(name);
     }
 }
