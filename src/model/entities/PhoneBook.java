21356da64043b2414dc8657929acd6ce3b65bb05
gabrielccsantos <gabrielcarneirocsantos@gmail.com>
2023-11-23 21:41:38 -0300

    Mudanças de logica e mais erro

--- src/model/entities/PhoneBook.java
+++ src/model/entities/PhoneBook.java
@@ -3,38 +3,16 @@ package model.entities;
 import model.contactException.ContactException;
 
 public class PhoneBook {
-    private Contact[] listContacts;
+    private final Contact[] listContacts;
 
 
     public PhoneBook(){
-        listContacts = new Contact[5];
+        listContacts = new Contact[3];
     }
 
     public void addContact(Contact contact){
-        boolean emptyList = true;
-
-        for(int i = 0; i < listContacts.length; i++){
-            if(listContacts[i] != null){
-                listContacts[i] = contact;
-                emptyList = false;
-            }
-        }
-        emptyList = true;
-
-        if(emptyList){
-            throw new ContactException("Lista está cheia");
-        }
     }
 
     public String showContact(String name){
-        for(int i = 0; i < listContacts.length; i++){
-            if(listContacts[i] != null){
-                if(listContacts[i].getName().equals(name)){
-                    String contactName = listContacts[i].getName();
-                    return contactName;
-                }
-            }
-        }
-        throw new ContactException("Não existe esse contato");
     }
 }
