f0fe12e83731be0ad24099a6e600030f80ff8454
Gabriel Santos <gabrielcarneirocsantos@gmail.com>
2023-11-24 12:29:52 -0300

    NNao lanca excecoes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,6 @@
 package application;
 
-import model.contactException.ContactException;
+import model.exception.ContactException;
 import model.entities.Contact;
 import model.entities.PhoneBook;
 
@@ -8,5 +8,58 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int choice;
 
+        PhoneBook phoneBook = new PhoneBook();
+
+        try {
+
+            do {
+                System.out.println("Escolha entre as opções abaixo: ");
+                System.out.println("1 - Procurar contato");
+                System.out.println("2 - Cadastrar contato");
+                System.out.println("0 - Sair do programa");
+                System.out.print("Digite uma opção: ");
+                choice = read.nextInt();
+                read.nextLine();
+
+                try {
+                    switch (choice) {
+                        case 1:
+                            System.out.print("Digite o nome que deseja procurar: ");
+                            String nameToSearch = read.nextLine();
+
+                            String result = phoneBook.showContact(nameToSearch);
+
+                            System.out.println("Phone de " + nameToSearch + ":" + result);
+
+                            break;
+                        case 2:
+                            System.out.print("Nome: ");
+                            String nameContact = read.nextLine();
+                            System.out.print("Phone: ");
+                            String phoneContact = read.nextLine();
+
+                            phoneBook.addContact(new Contact(nameContact, phoneContact));
+
+                            System.out.println("Cadastro feito com sucesso");
+
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            System.out.println("Entrada inválida");
+                    }
+                }catch (ContactException ce) {
+                    ce.getMessage();
+                }
+            }while (choice != 0);
+        }catch (ContactException ce){
+            ce.getMessage();
+        }finally {
+            System.out.println("Programa finalizado com sucesso");
+            read.close();
+        }
+    }
 }
